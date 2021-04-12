7e169be7b65cead35b1dc4b982752b0261f65ff3
Aleksandr <fortudie@gmail.com>
2021-04-12 03:36:55 +0300

    Инициализация проекта

---
+++ Wiki/Wiki/Pages/ContentPage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace Wiki.Pages
+{
+    /// <summary>
+    /// Основной контент статьи на сайте
+    /// </summary>
+    public class ContentPage
+    {
+        private readonly IWebDriver _driver;
+
+        public ContentPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        
+        /// <summary>
+        /// Заголовок статьи
+        /// </summary>
+        public IWebElement ContentHeader => _driver.FindElement(By.Id("firstHeading"));
+        
+        /// <summary>
+        /// Корневой элемент с данными статьи
+        /// </summary>
+        private IWebElement BodyContentRootElement => _driver.FindElement(By.XPath(BodyContentRootElementPath));
+        private const string BodyContentRootElementPath = "//div[@id='mw-content-text']";
+        /// <summary>
+        /// Ссылки в текстовых полях
+        /// </summary>
+        public IEnumerable<IWebElement> ArticleTextLinks => _driver.FindElements(By.XPath(ArticleTextLinkPath));
+        private const string ArticleTextLinkPath = BodyContentRootElementPath + "//p//a[@href and @title]";
+        /// <summary>
+        /// Выбор первой ссылки в в контенте статьи
+        /// </summary>
+        /// <returns></returns>
+        public IWebElement GetFirstArticleLink()
+        {
+            return ArticleTextLinks.First();
+        }
+    }
+}
---
+++ Wiki/Wiki/WikiTestClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Wiki.Pages;
+
+namespace Wiki
+{
+    public class WikiTestClass
+    {
+        public static void Main()
+        {
+            var driver = Init();
+            CheckFollowingLinkQuantity(driver, "Философия");
+        }
+
+        /// <summary>
+        /// Инициализируем драйвер
+        /// </summary>
+        /// <returns></returns>
+        public static IWebDriver Init()
+        {
+            //Инит
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.PageLoadStrategy = PageLoadStrategy.Normal;
+            chromeOptions.AddArgument("incognito");
+
+            //chromeOptions.AddArguments("headless");
+
+            var driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, chromeOptions);
+            
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            driver.Navigate().GoToUrl("https://ru.wikipedia.org/");
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
+
+            return driver;
+        }
+
+
+        public static void CheckFollowingLinkQuantity(IWebDriver driver, string targetArticleName)
+        {
+            //Загружаем модель экрана со статьей
+            var contentPage = new ContentPage(driver);
+            //Инициализируем набор записей для логирования
+            var logList = new List<string>();
+            //Создаем переменную - счетчик
+            var counter = 0;
+            //Кликаем гиперссылку Случайная страница для старта последовательности
+            var leftBarElements = new LeftBarElements(driver);
+            leftBarElements.RandomPageLink.Click();
+
+            //Делаем запись в логе в случае попадания на первую страницу - целевую
+            if (contentPage.ContentHeader.Text == targetArticleName)
+            {
+                var logString = $"Шаг {counter} - {contentPage.ContentHeader.Text} - {driver.Url}";
+                logList.Add(logString);
+                File.WriteAllLines("report.txt", logList);
+                return;
+            }
+
+            //В цикле выполняем переходы до попадания на целевую страницу
+            while (contentPage.ContentHeader.Text != targetArticleName)
+            {
+                //Увеличиваем значение счетчика
+                counter++;
+                //Делаем запись в лог
+                var logString = $"Шаг {counter} - {contentPage.ContentHeader.Text} - {driver.Url}";
+                logList.Add(logString);
+                //Кликаем на первую ссылку в данных статьи
+                contentPage = new ContentPage(driver);
+                contentPage.GetFirstArticleLink().Click();
+            }
+
+            //Записать результат в файл
+            File.WriteAllLines("report.txt", logList);
+        }
+    }
+}
